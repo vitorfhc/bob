a41d17e9a449c551050975c82221a81e54a212ae
Vitor Falcao <vitorfhc@protonmail.com>
2022-03-19 18:24:58 -0300

    Implements tests for helpers/fs

--- pkg/helpers/fs/fs.go
+++ pkg/helpers/fs/fs.go
@@ -3,7 +3,7 @@ package fs
 import (
 	"io/ioutil"
 	"os"
-	"path/filepath"
+	"strings"
 )
 
 // FileExists checks if <filename> is a file and if it exists.
@@ -17,24 +17,21 @@ func FileExists(filename string) bool {
 // If no file is found, it returns an empty string and an os.ErrNotExist error.
 func FindFileWithExtensions(filename string, extensions []string) (string, error) {
 	for _, ext := range extensions {
-		if FileExists(filename + ext) {
-			return filename + ext, nil
+		searchFilename := filename
+		if strings.HasSuffix(filename, ext) {
+			searchFilename = filename[:len(filename)-len(ext)]
+		}
+		if FileExists(searchFilename + ext) {
+			return searchFilename + ext, nil
 		}
 	}
 	return "", os.ErrNotExist
 }
 
-// GetFileWithoutExt returns the file name without the extension.
-func GetFileWithoutExt(filename string) string {
-	ext := filepath.Ext(filename)
-	return filename[:len(filename)-len(ext)]
-}
-
 // ReadYamlFile tries [.yml, .yaml] extensions to read a file and returns its
 // contents as a byte slice.
 func ReadYamlFile(filename string) ([]byte, error) {
-	fileNoExt := GetFileWithoutExt(filename)
-	file, err := FindFileWithExtensions(fileNoExt, []string{".yaml", ".yml"})
+	file, err := FindFileWithExtensions(filename, []string{".yaml", ".yml"})
 	if err != nil {
 		return nil, err
 	}
