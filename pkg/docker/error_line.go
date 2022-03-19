5b3f7078b0dcfa356811ffee106a44d61b5c9cb0
Vitor Falcao <vitorfhc@protonmail.com>
2022-03-19 16:48:41 -0300

    Adds status field to OutputLine

--- pkg/docker/error_line.go
+++ pkg/docker/error_line.go
@@ -6,13 +6,17 @@ import "encoding/json"
 type OutputLine struct {
 	Error  string `json:"error"`
 	Stream string `json:"stream"`
+	Status string `json:"status"`
 }
 
 func (ol *OutputLine) String() string {
 	if ol.Error != "" {
 		return ol.Error
 	}
-	return ol.Stream
+	if ol.Stream != "" {
+		return ol.Stream
+	}
+	return ol.Status
 }
 
 // HasError returns true if the line has an error.
